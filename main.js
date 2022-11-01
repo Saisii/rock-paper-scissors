1a0df17720d3c38c8335976004212a3f898b50c8
Saisii <sethconger@gmail.com>
2022-11-01 12:47:08 +0300

    complete playRound function

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let computerChoice = function getComputerChoice () {
+function getComputerChoice () {
     let outcome = Math.floor(Math.random() *3);
     if (outcome === 0) {
         return "rock";
@@ -11,7 +11,46 @@ let computerChoice = function getComputerChoice () {
     }
 }
 
-console.log(computerChoice());
 
+
+function playRound (player, computer) {
+    console.log(computer);
+    if (player.toLowerCase() === computer) {
+        return `You chose ${player.toLowerCase()}. The computer chose ${computer}. It's a tie!`;
+    }
+
+    else if (player.toLowerCase() === "rock" && computer == "paper") {
+        return `You chose ${player.toLowerCase()}. The computer chose ${computer}. You lose!`;
+    }
+
+    else if (player.toLowerCase() === "rock" && computer == "scissors") {
+        return `You chose ${player.toLowerCase()}. The computer chose ${computer}. You win!`;
+    }
+
+    else if (player.toLowerCase() === "paper" && computer == "scissors") {
+        return `You chose ${player.toLowerCase()}. The computer chose ${computer}. You lose!`;
+    }
+
+    else if (player.toLowerCase() === "paper" && computer == "rock") {
+        return `You chose ${player.toLowerCase()}. The computer chose ${computer}. You win!`;
+    }
+
+    else if (player.toLowerCase() === "scissors" && computer == "rock") {
+        return `You chose ${player.toLowerCase()}. The computer chose ${computer}. You lose!`;
+    }
+
+    else if (player.toLowerCase() === "scissors" && computer == "paper") {
+        return `You chose ${player.toLowerCase()}. The computer chose ${computer}. You win!`;
+    }
+
+    else {
+        return "something went wrong    ";
+    }
+
+}
+
+const playerSelection = "Rock";
+
+console.log(playRound(playerSelection, getComputerChoice()));
 
 
